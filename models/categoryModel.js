f2a0507ca1ff4c1316cd959e4e3ce6e1c588cc51
Hermann Krohn <krohnhermannacosta@gmail.com>
2019-08-08 15:03:45 -0500

    hermann changed the file structure

--- models/categoryModel.js
+++
@@ -1,9 +0,0 @@
-const connection = require('../DB/knexConnection')
-
-class categoryModel {
-    static findItemCategory(itemID){
-        return connection.select('category').from('categories').where('itemID', itemID)
-    }
-}
-
-module.exports = categoriesModel
--- models/pantryItemModel.js
+++
@@ -1,9 +0,0 @@
-const connection = require('../DB/knexConnection')
-
-class pantryItemModel {
-    static findAllPantryItems(userID){
-        return connection.select().from('pantryItems').where('userID', userID)
-    }
-}
-
-module.exports = pantryItemModel
